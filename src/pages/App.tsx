f0c922ffa599b9bd606b992fcc061291d0259777
onilson-csf <onilson.cardoso@gmail.com>
2024-02-20 17:06:44 -0300

    initial

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Formulario from './components/Formulario';
-import Lista from './components/Lista';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppStyle">
       <Formulario />
       <Lista />
     </div>
